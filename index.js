3070495b59ac8bdfa470619e117d4b493ed767de
jadentheace <119875706+jadentheace@users.noreply.github.com>
2026-02-23 19:43:14 -0600

    Refactor server port and remove WebSocket handling
    Updated server port configuration and removed old WebSocket logic.

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-const Net = require('net');
-const WebSocket = require('ws');
-
-// REDIRECTING FROM ZPOOL TO MINING-DUTCH
-const POOL_HOST = 'dash.mining-dutch.nl';
-const POOL_PORT = 3333; 
-
-const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
-
-wss.on('connection', (ws) => {
-    const poolSocket = new Net.Socket();
-    
-    poolSocket.connect(POOL_PORT, POOL_HOST, () => {
-        console.log("SUCCESS: Connected to Mining-Dutch X11");
-    });
-
-    ws.on('message', (data) => {
-        poolSocket.write(data + '\n');
-    });
-
-    poolSocket.on('data', (data) => {
-        ws.send(data.toString());
-    });
-
-    ws.on('close', () => poolSocket.destroy());
-    poolSocket.on('error', (err) => {
-        console.log("Pool Error: " + err.message);
-        ws.terminate();
-    });
+const port = process.env.PORT || 4444; // Render tells the app which port to use via process.env.PORT
+server.listen(port, () => {
+    console.log(`Miner Proxy is running on internal port ${port}`);
 });
-
-console.log("DASH PROXY ONLINE");
