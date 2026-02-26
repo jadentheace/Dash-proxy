43b759fd146334fc72c9dd282922f5ce56bde74d
jadentheace <119875706+jadentheace@users.noreply.github.com>
2026-02-26 16:22:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const net = require('net');
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 wss.on('connection', (ws) => {
-    // HARD-CODED TO ZPOOL VERUSHASH PORT 3300
-    const client = new net.Socket();
-    client.connect(3300, 'verushash.mine.zpool.ca', () => {
-        console.log('Zpool Connection Secured');
+    // FORCE CONNECTION TO ZPOOL VERUSHASH PORT
+    const zpool = net.connect(3300, 'verushash.mine.zpool.ca', () => {
+        console.log('CONNECTED TO ZPOOL STRATUM');
     });
 
-    ws.on('message', (msg) => {
-        client.write(msg + '\n');
+    ws.on('message', (data) => {
+        // Ensure every message ends with a newline for Stratum protocol
+        zpool.write(data + '\n');
     });
 
-    client.on('data', (data) => {
+    zpool.on('data', (data) => {
         ws.send(data.toString());
     });
 
-    client.on('close', () => ws.close());
-    ws.on('close', () => client.destroy());
+    zpool.on('error', (err) => console.log('ZPOOL_ERR:', err.message));
+    ws.on('close', () => zpool.destroy());
+    zpool.on('close', () => ws.close());
 });
